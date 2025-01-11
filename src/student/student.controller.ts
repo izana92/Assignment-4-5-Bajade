a2cb26776675859d50011970cda18c62633b1f75
Jheric Bajade <jheric.bajade@cvsu.edu.ph>
2025-01-11 18:42:35 +0800

    create student

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -7,39 +7,11 @@ import { Student } from './student.entity';
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  // CREATE
+  // Create
   @Post()
   create(@Body() student: Partial<Student>) {
     return this.studentService.createStudent(student);
   }
 
-  // READ (GET ALL)
-  @Get()
-  findAll() {
-    return this.studentService.getAllStudents();
-  }
-
-  // READ (GET ONE)
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.studentService.getStudentById(id);
-  }
-
-  // UPDATE (PUT)
-  @Put(':id')
-  update(@Param('id') id: number, @Body() student: Partial<Student>) {
-    return this.studentService.updateStudent(id, student);
-  }
-
-  // PARTIAL UPDATE (PATCH)
-  @Patch(':id')
-  partialUpdate(@Param('id') id: number, @Body() student: Partial<Student>) {
-    return this.studentService.partialUpdateStudent(id, student);
-  }
-
-  // DELETE
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.studentService.deleteStudent(id);
-  }
+  
 }
