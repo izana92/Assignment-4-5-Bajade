cb16930aeeef0d87a727a0f45490975a71e31101
Jheric Bajade <jheric.bajade@cvsu.edu.ph>
2025-01-11 18:19:20 +0800

    create student

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -7,34 +7,10 @@ import { Student } from './student.entity';
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  
-  // READ (GET ALL)
-  @Get()
-  findAll() {
-    return this.studentService.getAllStudents();
+  // Create
+  @Post()
+  create(@Body() student: Partial<Student>) {
+    return this.studentService.createStudent(student);
   }
 
-  // READ (GET ONE)
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.studentService.getStudentById(id);
-  }
-
-  // UPDATE (PUT)
-  @Put(':id')
-  update(@Param('id') id: number, @Body() student: Partial<Student>) {
-    return this.studentService.updateStudent(id, student);
-  }
-
-  // PARTIAL UPDATE (PATCH)
-  @Patch(':id')
-  partialUpdate(@Param('id') id: number, @Body() student: Partial<Student>) {
-    return this.studentService.partialUpdateStudent(id, student);
-  }
-
-  // DELETE
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.studentService.deleteStudent(id);
-  }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -10,41 +10,10 @@ export class StudentService {
     private studentRepository: Repository<Student>,
   ) {}
 
-
-  // READ (GET ALL)
-  async getAllStudents(): Promise<Student[]> {
-    return await this.studentRepository.find();
+  // Create
+  async createStudent(student: Partial<Student>): Promise<Student> {
+    const newStudent = this.studentRepository.create(student);
+    return await this.studentRepository.save(newStudent);
   }
 
-  // READ (GET ONE)
-  async getStudentById(id: number): Promise<Student> {
-    const student = await this.studentRepository.findOne({ where: { id } });
-    if (!student) {
-      throw new NotFoundException('Student not found');
-    }
-    return student;
-  }
-
-  // UPDATE (PUT)
-  async updateStudent(id: number, updatedData: Partial<Student>): Promise<Student> {
-    const student = await this.getStudentById(id);
-    Object.assign(student, updatedData);
-    return this.studentRepository.save(student);
-  }
-
-  // PARTIAL UPDATE (PATCH)
-  async partialUpdateStudent(id: number, partialData: Partial<Student>): Promise<Student> {
-    const student = await this.getStudentById(id);
-    Object.assign(student, partialData);
-    return this.studentRepository.save(student);
-  }
-
-  // DELETE
-  async deleteStudent(id: number): Promise<{ message: string }> {
-    const result = await this.studentRepository.delete(id);
-    if (result.affected === 0) {
-      throw new NotFoundException('Student not found');
-    }
-    return { message: `Student with ID ${id} has been deleted` };
-  }
 }
