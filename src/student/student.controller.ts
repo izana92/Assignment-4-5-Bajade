f4f2140d47a5d117728e0ea3fc4ed19eacd128bd
Jheric Bajade <jheric.bajade@cvsu.edu.ph>
2025-01-11 18:03:54 +0800

    create student

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,3 +1,4 @@
+
 import { Controller, Post, Get, Put, Patch, Delete, Body, Param } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from './student.entity';
@@ -6,8 +7,11 @@ import { Student } from './student.entity';
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.studentService.deleteStudent(id);
+  //Create 
+  @Post()
+  create(@Body() student: Partial<Student>) {
+    return this.studentService.createStudent(student);
   }
+
+ 
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -10,11 +10,10 @@ export class StudentService {
     private studentRepository: Repository<Student>,
   ) {}
 
-  async deleteStudent(id: number): Promise<{ message: string }> {
-    const result = await this.studentRepository.delete(id);
-    if (result.affected === 0) {
-      throw new NotFoundException('Student not found');
-    }
-    return { message: `Student with ID ${id} has been deleted` };
+  //Create
+  async createStudent(student: Partial<Student>): Promise<Student> {
+    const newStudent = this.studentRepository.create(student);
+    return await this.studentRepository.save(newStudent);
   }
+
 }
